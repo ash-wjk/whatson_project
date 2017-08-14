ff79fadadc427e23f1b79a1584300c49c07af672
pinky-coder <emailtoasha.wjk@gmail.com>
2017-08-14 02:34:01 +0100

    added add-event activity

---
+++ WhatsOn/app/src/main/java/uk/co/ashawijekoon/whatson/activities/AddEventActivity.java
@@ -0,0 +1,15 @@
+package uk.co.ashawijekoon.whatson.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.ashawijekoon.whatson.R;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
--- WhatsOn/app/src/main/java/uk/co/ashawijekoon/whatson/activities/MainActivity.java
+++ WhatsOn/app/src/main/java/uk/co/ashawijekoon/whatson/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package uk.co.ashawijekoon.whatson.activities;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,6 +18,7 @@ import uk.co.ashawijekoon.whatson.models.Event;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private FloatingActionButton mAddEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListView = (ListView) findViewById(R.id.events_list_view);
+        mAddEventButton = (FloatingActionButton) findViewById(R.id.addEventActionButton);
 
         final ArrayList<Event> eventList = getEvents();
         EventAdapter adapter = new EventAdapter(this, eventList);
         mListView.setAdapter(adapter);
 
+        mAddEventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                OpenAddEvent();
+            }
+        });
+
+    }
+
+    private void OpenAddEvent(){
+        Intent intent = new Intent(this, AddEventActivity.class);
+        startActivity(intent);
     }
 
     public ArrayList<Event> getEvents(){
